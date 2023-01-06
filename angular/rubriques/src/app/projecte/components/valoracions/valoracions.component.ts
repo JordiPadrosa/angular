856047aa577de55bc6e1bfbdaa607deab8a5005d
JordiPadrosa <jpadrosa8@boscdelacoma.cat>
2023-01-06 13:01:29 +0100

    valoracions Form

--- angular/rubriques/src/app/projecte/components/valoracions/valoracions.component.ts
+++ angular/rubriques/src/app/projecte/components/valoracions/valoracions.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Criteri } from '../../model/entitats/implementacions/Criteri';
+import { Valoracio } from '../../model/entitats/implementacions/Valoracio';
 
 @Component({
   selector: 'app-valoracions',
@@ -7,17 +9,45 @@ import { Criteri } from '../../model/entitats/implementacions/Criteri';
   styleUrls: ['./valoracions.component.css']
 })
 export class ValoracionsComponent implements OnInit {
+  valoracionsForm!: FormGroup;
   criterisLocalStorage!: string | null;
   criteris: Array<Criteri> = [];
-  constructor() {
+  selectedOption?: string;
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.criterisLocalStorage = localStorage.getItem('criteris');
     if(this.criterisLocalStorage != null){
       this.criteris = JSON.parse(this.criterisLocalStorage);
-      console.log(this.criteris);
     }
+    this.valoracionsForm = this.fb.group({
+      descripcio: ['',
+        {
+          validators: [
+            Validators.required,
+          ]
+        }
+      ],
+      valor: ['',
+        {
+          validators: [
+            Validators.required,
+          ]
+        }
+      ],
+  })
+  this.selectedOption = this.criteris[0].titol;
+}
+  afegirValoracio(): void {
+    for(var i = 0; i < this.criteris.length; i++){
+      if(this.criteris[i].titol == this.selectedOption){
+        this.criteris[i].valoracions.push(this.valoracionsForm.get("descripcio")?.value, this.valoracionsForm.get("valor")?.value);
+      }
+    }
+    localStorage.setItem('criteris', JSON.stringify(this.criteris));
+    console.log(this.criteris);
   }
+  
 
 }
