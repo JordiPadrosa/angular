77a20c795fba48707e32cb83b0384138e7a841d2
JordiPadrosa <jpadrosa8@boscdelacoma.cat>
2023-01-09 20:01:44 +0100

    taula rubriques

--- angular/rubriques/src/app/projecte/components/rubriques/rubriques.component.ts
+++ angular/rubriques/src/app/projecte/components/rubriques/rubriques.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Criteri } from '../../model/entitats/implementacions/Criteri';
+import { Valoracio } from '../../model/entitats/implementacions/Valoracio';
 
 @Component({
   selector: 'app-rubriques',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rubriques.component.css']
 })
 export class RubriquesComponent implements OnInit {
-
+  criteris: Array<Criteri> = [];
+  criterisLocalStorage!: string | null;
+  valoracio!: Valoracio;
+  numeros: Array<number> = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.criterisLocalStorage = localStorage.getItem('criteris');
+    if(this.criterisLocalStorage != null){
+      this.criteris = JSON.parse(this.criterisLocalStorage);
+    }
+    this.numeros = this.getNumeros();
   }
+
+  getNumeros() {
+    this.criteris.forEach(criteri => {
+      for(let i = 0; i < criteri.valoracions.length; i++) {
+        if(!this.numeros.includes(criteri.valoracions[i].valor) && criteri.valoracions[i].valor != 0){
+          this.numeros.push(criteri.valoracions[i].valor);
+        }
+      }
+    });
+    return this.numeros.sort();
+  }
+
+  ordenarValoracions(criteri: Criteri, num: number): String {
+    this.valoracio = new Valoracio('', 0);
+    for(let i = 0; i < criteri.valoracions.length; i++) {
+      if(criteri.valoracions[i].valor == num){
+        this.valoracio = criteri.valoracions[i];
+      }
+    }
+    return this.valoracio.descripcio;
+  }
+
 
 }
